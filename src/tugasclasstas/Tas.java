42d3eaec7e2d1f4a0da26576600b51d2e1999ff3
desyfajrianti <dessy.kps@gmail.com>
2018-03-27 17:31:23 +0800

    Membuat atribut dan Behavior

--- src/tugasclasstas/Tas.java
+++ src/tugasclasstas/Tas.java
@@ -10,5 +10,17 @@ package tugasclasstas;
  * @author my computer
  */
 public class Tas {
+    String jenis;
+    String bahan;
+    String warna;
+    String merk;
+    String harga; 
     
+    void cetakInfo(){
+        System.out.println("Botol      :"+jenis);
+        System.out.println("Volume Air :"+bahan);
+        System.out.println("Rasa       :"+warna);
+        System.out.println("Warna      :"+merk);
+        System.out.println("Harga      :"+harga);
+    }
 }
